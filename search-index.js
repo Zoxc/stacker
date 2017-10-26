a410674c3b4ab3548205a212cfb4d04bd8003c28
Travis CI User <travis@example.org>
2017-10-26 18:38:21 +0000

    Documentation for alexcrichton/stacker@b0f3ccd

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["stacker"] = {"doc":"A library to help grow the stack when it runs out of space.","items":[[5,"maybe_grow","stacker","Grows the call stack if necessary.",null,{"inputs":[{"name":"usize"},{"name":"usize"},{"name":"f"}],"output":{"name":"r"}}],[5,"remaining_stack","","Queries the amount of remaining stack as interpreted by this library.",null,{"inputs":[],"output":{"name":"usize"}}]],"paths":[]};
+initSearch(searchIndex);
